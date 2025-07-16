ad10356ffb82560f7d08d548fa50504398f3dc7e
mac <ahuekweprinceugo@gmail.com>
2025-07-17 00:38:05 +0100

    ui update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,24 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+/** @format */
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Imo State Ministry Of Works",
-  description: "The Official Site Of The Imo State Ministry Of Works",
-  favicon: "../../../../public/imo-logo2.jpg",
+  title: 'Imo State Ministry Of Works',
+  description: 'The Official Site Of The Imo State Ministry Of Works',
+  favicon: '../../../../public/imo-logo2.jpg',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
