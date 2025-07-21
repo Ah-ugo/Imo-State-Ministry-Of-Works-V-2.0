c2948852cdb103bb40791c7d5b033cccf0436613
mac <ahuekweprinceugo@gmail.com>
2025-07-21 01:34:07 +0100

    first nextjs api

--- src/app/api/docs/page.js
+++ src/app/api/docs/page.js
@@ -2,9 +2,13 @@
 
 'use client';
 
-import SwaggerUI from 'swagger-ui-react';
+import dynamic from 'next/dynamic';
 import 'swagger-ui-react/swagger-ui.css';
 import { useEffect, useState } from 'react';
+
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), {
+  ssr: false,
+});
 
 export default function SwaggerPage() {
   const [spec, setSpec] = useState(null);
